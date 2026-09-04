3316c7514ad5738847ba63a92fcec99a95c25964
thecosmicorder <thecosmicorder@gmail.com>
2026-09-04 16:42:41 +0800

    Fix EchoGarden feedback buffer initializer

--- AuroraEchoGarden.cpp
+++ AuroraEchoGarden.cpp
@@ -24,7 +24,7 @@ DelayLine<float, kMaxDelaySamples> DSY_SDRAM_BSS delay_lines[kNumLines];
 ReverbSc DSY_SDRAM_BSS reverb;
 
 float smooth_delay[kNumLines] = {24000.0f, 32000.0f, 40000.0f, 48000.0f};
-float feedback_lp[kNumLines]  = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+float feedback_lp[kNumLines]  = {0.0f, 0.0f, 0.0f, 0.0f};
 
 bool freeze_latched   = false;
 bool pingpong_latched = false;
