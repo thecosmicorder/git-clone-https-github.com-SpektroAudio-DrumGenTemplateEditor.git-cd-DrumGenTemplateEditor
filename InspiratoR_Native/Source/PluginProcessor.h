867727a32efb5e0aa2d608e29bfb9351b39c285e
thecosmicorder <thecosmicorder@gmail.com>
2026-09-09 18:06:42 +0800

    Report VST3 wrapper as instrument rather than pure MIDI effect

--- InspiratoR_Native/Source/PluginProcessor.h
+++ InspiratoR_Native/Source/PluginProcessor.h
@@ -24,7 +24,7 @@ public:
     const juce::String getName() const override { return JucePlugin_Name; }
     bool acceptsMidi() const override { return true; }
     bool producesMidi() const override { return true; }
-    bool isMidiEffect() const override { return true; }
+    bool isMidiEffect() const override { return false; }
     double getTailLengthSeconds() const override { return 0.0; }
 
     int getNumPrograms() override { return 1; }
